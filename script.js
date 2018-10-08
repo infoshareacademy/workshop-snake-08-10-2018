444f35340748c3257547114ad2d3c46fc35b277b
Mateusz Choma <mateusz.choma@infoshareacademy.com>
2018-10-08 18:21:51 +0200

    Rendering board - part 1

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('Hello world!!!')
+const _board = [
+    [1, 1, 1, 1],
+    [1, 1, 'H', 1],
+    [1, 1, 0, 1],
+    [1, 0, 0, 1]
+]
+
+const renderBoard = () => {
+    _board.forEach(
+        (row, i, a) => renderRow(row)
+    )
+}
+
+const renderRow = (row) => {
+    row.forEach(
+        (cell, i, a) => renderCell(cell)
+    )
+}
+
+const renderCell = (cell) => {
+    console.log(cell)
+}
+
+renderBoard()
